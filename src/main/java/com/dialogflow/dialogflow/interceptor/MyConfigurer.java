6665b1b37cbd3164bec363c15cfca6a5cb0e192a
fexiong <fexiong@ebay.com>
2022-03-15 14:12:24 +0800

    add interceptor

--- src/main/java/com/dialogflow/dialogflow/interceptor/MyConfigurer.java
+++ src/main/java/com/dialogflow/dialogflow/interceptor/MyConfigurer.java
@@ -1,9 +1,11 @@
 package com.dialogflow.dialogflow.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class MyConfigurer implements WebMvcConfigurer {
 
     @Autowired
@@ -11,7 +13,6 @@ public class MyConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-     //   registry.addInterceptor(authInterceptor).addPathPatterns("/**");
-        System.out.println("WWWWWWWWW");
+        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
     }
 }
